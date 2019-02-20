5758da73c81c0cc01366d54d11863fff9daa6f31
WeronikaDebowska <37541458+WeronikaDebowska@users.noreply.github.com>
2019-02-20 20:41:30 +0100

    Delete MoveValidatorService.java

--- src/main/java/com/codecool/scrabble/Service/MoveValidatorService.java
+++
@@ -1,8 +0,0 @@
-package com.codecool.scrabble.Service;
-
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-
-public interface MoveValidatorService {
-    LinkedList<String> checkMoveValidity();
-}
